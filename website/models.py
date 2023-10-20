6114c57879e5b9de77f51a716b12ac8806bedfa1
Jeff Morales <jeffmorales29@gmail.com>
2023-10-21 03:29:09 +0800

    View record

--- website/models.py
+++ website/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
